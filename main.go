8ce2f7b625e9b7c62a63bed085f4dce493d70193
bulatok <bulat2020205@gmail.com>
2021-09-16 22:40:41 +0300

    SecondCommit

--- main.go
+++ main.go
@@ -2,37 +2,10 @@ package main
 
 import (
 	"fmt"
-	//"bufio"
-	//"os"
-	//"strings"
 )
-
-//type Car struct{
-//	speed int
-//	name string
-//	dsl string
-//}
-//type Moto struct{
-//	mv int
-//	name string
-//	vvs string
-//}
-//
-//type Rider interface {
-//	MoveSpeed(int) error
-//}
-//func (m *Moto) MoveSpeed(int speed) error{
-//	m.mv -= speed
-//	return nil
-//}
-//
-//func (c *Car) MoveSpeed(int speed) error{
-//	c.speed -= speed
-//	return nil
-//}
 func main() {
-	const s = 'a'
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%+q\n", s)
-	fmt.Printf("%U\n", s)
+	var n int = 21
+	fmt.Printf("%T %T %T\n", n, int32(n), string(n))
+	fmt.Printf("%t %t %t\n", n, int32(n), string(n))
 }
+
