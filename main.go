c4a809b40f0bc5a99e2eab8d533b10bbfcd89b79
bulatok <bulat2020205@gmail.com>
2021-09-18 00:49:39 +0300

    f4

--- main.go
+++ main.go
@@ -3,82 +3,66 @@ package main
 import (
 	"fmt"
 	//"log"
-	"sort"
 	"os"
+	"sort"
 )
 
 func IgnoreDirs(s string) bool {
-	return (s == ".git")
+	return (false) // конкретно сейчас нечего игнорить
 }
-func ToString(x int64) string{
+func ToString(x int64) string {
 	return fmt.Sprintf("%s", x)
 }
-//const way = "../../"
+
 const way = "C:/Users/kutlu/OneDrive/Рабочий стол/hw1_tree"
-func Govno(tabs int, last bool){
-	for i := 0; i < tabs-1; i++{
-		for j:=0; j < 4; j++{
+
+func Govno(tabs int, last bool) {
+	for i := 0; i < tabs-1; i++ {
+		for j := 0; j < 4; j++ {
 			fmt.Printf(" ")
 		}
 	}
 	if last == false {
 		fmt.Printf("├")
-	} else{
+	} else {
 		fmt.Printf("└")
 	}
-		for j:=0; j < 3; j++ {
+	for j := 0; j < 3; j++ {
 		fmt.Printf("─")
 	}
 }
 func recurs(entry []os.DirEntry, sway string, tabs int) {
 	// чтобы вывести всё было отсортированным dude
-	sort.Slice(entry, func(i, j int) bool{
+	sort.Slice(entry, func(i, j int) bool {
 		return entry[i].Name() < entry[j].Name()
 	})
-	for i, v := range entry{
-		if IgnoreDirs(v.Name()){
+	for i, v := range entry {
+		if IgnoreDirs(v.Name()) {
 			continue
 		}
 
-		Govno(tabs, (i == len(entry) - 1))
+		Govno(tabs, (i == len(entry)-1))
 
+		// вывод имени файла + его размер
 		infa, _ := v.Info()
-		if infa.Size() == 0{
+		if infa.Size() == 0 {
 			fmt.Printf("%s (empty) \n", v.Name())
-		}else{
+		} else {
 			fmt.Printf("%s (%db) \n", v.Name(), infa.Size())
 		}
 
-		if v.IsDir() == true{
+		if v.IsDir() == true {
 			nxt, _ := os.ReadDir(sway + "/" + v.Name())
 			tabs += 1
-			recurs(nxt, sway + "/" + v.Name(), tabs)
+			recurs(nxt, sway+"/"+v.Name(), tabs)
 			tabs -= 1
 		}
 	}
 }
-
 
 func main() {
 
 	start, _ := os.ReadDir(way)
 	recurs(start, way, 1)
 
-
-	//d, err := os.ReadDir(way)
-	//if err != nil{
-	//	fmt.Println(err)
-	//	return
-	//}
-	//for _, f := range d{
-	//	nxt, _ := os.ReadDir(way + f.Name())
-	//	if f.Name() == ".git"{
-	//		continue
-	//	}
-	//	fmt.Printf("%s\n", f.Name())
-	//	for _, n := range nxt{
-	//		fmt.Printf("\t%s\n", n.Name())
-	//
-	//	}
-	//}
 }
