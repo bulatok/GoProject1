09e0d9b6f321fd235b83c9c61881cf764ad4a839
bulatok <bulat2020205@gmail.com>
2021-09-17 23:06:13 +0300

    17.09.2021 23:06:12

--- main.go
+++ main.go
@@ -2,10 +2,65 @@ package main
 
 import (
 	"fmt"
+	//"log"
+	//"sort"
+	"os"
 )
-func main() {
-	var n int = 21
-	fmt.Printf("%T %T %T\n", n, int32(n), string(n))
-	fmt.Printf("%t %t %t\n", n, int32(n), string(n))
+
+func IsDoteGit(s string) bool {
+	return (s == ".git" || s == "pkg")
 }
 
+const way = "../../"
+
+func recurs(entry []os.DirEntry, sway string, tabs int) {
+	for _, v := range entry{
+		if IsDoteGit(v.Name()){
+			continue
+		} // checking that file is not .git
+
+		for i := 0; i < tabs; i++{
+			for j:=0; j < 4; j++{
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Printf("%s\n", v.Name())
+
+		if v.IsDir() == true{
+			//fmt.Printf("/")
+			nxt, _ := os.ReadDir(sway + "/" + v.Name())
+			tabs += 1
+			recurs(nxt, sway + "/" + v.Name(), tabs)
+			tabs -= 1
+		}
+	}
+}
+
+
+func main() {
+	//g, _ := os.ReadDir("../../pkg")
+	//for _, v := range(g){
+	//	fmt.Println(v.Name())
+	//}
+
+	start, _ := os.ReadDir(way)
+	recurs(start, way, 0)
+
+
+	//d, err := os.ReadDir(way)
+	//if err != nil{
+	//	fmt.Println(err)
+	//	return
+	//}
+	//for _, f := range d{
+	//	nxt, _ := os.ReadDir(way + f.Name())
+	//	if f.Name() == ".git"{
+	//		continue
+	//	}
+	//	fmt.Printf("%s\n", f.Name())
+	//	for _, n := range nxt{
+	//		fmt.Printf("\t%s\n", n.Name())
+	//
+	//	}
+	//}
+}
